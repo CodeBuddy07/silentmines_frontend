aaf5eab124fda24231aa357bb92aaa5424d6fd72
Ruhul <brainboostservices@gmail.com>
2025-09-18 21:03:40 +0600

    null commit

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <footer className="bg-black border-t border-gray-800 text-gray-300">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 ">
         {/* Top Section */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {/* Branding */}
